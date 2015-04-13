37a23d6cea4fc5e48e33d0d1fd1cf6d1467444ed
Pascal van Kooten <pvkooten@jibes.nl>
2015-04-13 21:34:12 +0200

    comparison

--- sky/comparison.py
+++ sky/comparison.py
@@ -1,51 +1,46 @@
-tree = lxml.html.fromstring(re.sub('\s+', ' ', str(html)))
+############################ tree = lxml.html.fromstring(re.sub('\s+', ' ', str(html)))
 
-tree2 = lxml.html.fromstring(re.sub('\s+', ' ', html2))
+############################ tree2 = lxml.html.fromstring(re.sub('\s+', ' ', html2))
 
 import selenium
 from selenium import webdriver
-
 import lxml.html
-
-
-
-import lxml.html.diff as df
-
-
-
-from urllib.request import urlopen
-
+import lxml.html.diff
+from requests import urlopen
 from selenium.webdriver.common.keys import Keys
-
-tree2 = lxml.html.fromstring(html)
-
-
 
 def viewString(x, driver):
     with open('/tmp/seleniumStringPage.html', 'w') as f:
         f.write(x)
     driver.get('file:///tmp/seleniumStringPage.html')
 
-def testUrl(url): 
-    try:
-        driver = webdriver.Firefox()
-        driver.maximize_window()
-        driver.get(url)
-        html1 = str(urlopen(url).read()).replace('\\n','').replace('\\r','').replace("\\'", "'").replace("\\t", "\t")
+def compareRequestsAndSelenium(url): 
+    html1 = str(requests.get(url).text)
+    try: 
+        driver = webdriver.PhantomJS()
+        driver.maximize_window() 
+        driver.get(url) 
         html2 = str(driver.page_source) 
-        tree = lxml.html.fromstring(html1)
-        tree2 = lxml.html.fromstring(html2)
-        diffHtml = df.htmldiff(tree, tree2)
-        diffTree = lxml.html.fromstring(diffHtml)
-        insCounts = diffTree.xpath('count(//ins)')
-        delCounts = diffTree.xpath('count(//del)')
-        pureDiff = ''
-        for x in ['.//ins', './/del']:
-            for y in [z for z in diffTree.iter() if z.tag in ['ins', 'del']]:
-                if y.text is not None:
-                    color = 'lightgreen' if 'ins' in y.tag else 'red'
-                    pureDiff += '<div style="background-color:{};">{}</div>'.format(color, y.text)
-        print('From non-javascript to javascript, {} insertions and {} deleted'.format(insCounts, delCounts))
+    finally:    
+        driver.close()
+    viewDiffHtml(url, html1, html2)    
+
+def viewDiffHtml(url, html1, html2, diffMethod = lxml.html.diff.htmldiff):
+    tree = lxml.html.fromstring(html1)
+    tree2 = lxml.html.fromstring(html2)
+    diffHtml = diffMethod(tree, tree2)
+    diffTree = lxml.html.fromstring(diffHtml)
+    insCounts = diffTree.xpath('count(//ins)')
+    delCounts = diffTree.xpath('count(//del)')
+    pureDiff = ''
+    for x in ['.//ins', './/del']:
+        for y in [z for z in diffTree.iter() if z.tag in ['ins', 'del']]:
+            if y.text is not None:
+                color = 'lightgreen' if 'ins' in y.tag else 'red'
+                pureDiff += '<div style="background-color:{};">{}</div>'.format(color, y.text)
+    print('From non-javascript to javascript, {} insertions and {} deleted'.format(insCounts, delCounts))
+    try:    
+        driver = webdriver.Firefox()
         diff = '<head><title>diff</title><base href=' + url + ' target="_blank"><style>ins{ background-color:lightgreen; } del{background-color:red;}</style></head>' +  diffHtml
         viewString(diff, driver) 
         driver.find_element_by_tag_name("body").send_keys(Keys.COMMAND + 't')
@@ -54,12 +49,13 @@ def testUrl(url):
         viewString(html2, driver) 
         driver.find_element_by_tag_name("body").send_keys(Keys.COMMAND + 't')
         viewString('<html><body>{}</body></html>'.format(str(pureDiff)), driver) 
-    finally:    
-        input('bla?')
+    finally:
+        input('bla?') 
         driver.close()
 
 url = 'http://www.healthgrades.com/physician/dr-jeannine-villella-y4jts'
-z=testUrl(url)    
+z=compareRequestsAndSelenium(url)    
+
 
 
 attrs = ' '.join(y.keys())
