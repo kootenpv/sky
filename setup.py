f9343e21a94a85d0f447c6b698f18ec252a2b977
Pascal van Kooten <kootenpv@gmail.com>
2015-08-17 23:53:57 +0200

    one more packinging attempt

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 MAJOR_VERSION = '0'
 MINOR_VERSION = '0'
-MICRO_VERSION = '101'
+MICRO_VERSION = '102'
 VERSION = "{}.{}.{}".format(MAJOR_VERSION, MINOR_VERSION, MICRO_VERSION)
 
 setup(name = 'sky',
